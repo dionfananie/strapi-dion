c9a4bbafffa6430e60f01eae629538c40f5fb500
dion.fananie <dion.fananie@BA-00017795.local>
2021-11-16 05:41:35 +0700

    add init strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6cf3336619d87608eea7dbeae8f6d956'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2e7310fe-c0a4-47d3-bba2-a8c33b3bbbd2'
+};
